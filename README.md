d9f8a3a07792995bfe4c13b49bd55e5989db23ae
Waywrong <wlhsu@ira.ee.ntu.edu.tw>
2017-07-29 21:44:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+gnuplot example
+============
+
+	mkdir build
+	cd build
+	cmake ..
+	make
+	./gnuplot_example
+
+
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv) {
+
+  FILE *command;
+  command = popen( "gnuplot", "w" ); // XP Vista 皆可
+  fprintf( command, "a=-1; b=-2; c=3;" );
+  fprintf( command, "plot a*x*x+b*x+c\n");
+  fflush( command );
+
+  fgetc(stdin);  
+  pclose( command );
+  return 0;
+}
