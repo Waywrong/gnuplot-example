bcd8dd4774857975d2f2f3594da6c43ea5d8bb81
Waywrong <wlhsu304@gmail.com>
2017-07-29 21:45:31 +0800

    remove comment

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
 
   FILE *command;
-  command = popen( "gnuplot", "w" ); // XP Vista 皆可
+  command = popen( "gnuplot", "w" ); 
   fprintf( command, "a=-1; b=-2; c=3;" );
   fprintf( command, "plot a*x*x+b*x+c\n");
   fflush( command );
