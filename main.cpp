a5ee6c0f63bac2b4a8be7655e184028ee50e981f
Waywrong <wlhsu304@gmail.com>
2017-07-29 22:36:20 +0800

    Windows version

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 int main(int argc, char **argv) {
 
   FILE *command;
-  command = popen( "gnuplot", "w" ); 
+  //command = _popen( "\"C:\\Program Files\\gnuplot\\bin\\gnuplot\"", "w" ); // XP可用，Vista不行
+  command = _popen( "pgnuplot", "w" ); // XP Vista 皆可
   fprintf( command, "a=-1; b=-2; c=3;" );
   fprintf( command, "plot a*x*x+b*x+c\n");
   fflush( command );
 
-  fgetc(stdin);  
-  pclose( command );
+  system("PAUSE");
+  _pclose( command );
   return 0;
 }
